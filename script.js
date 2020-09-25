65c9ceb20228bda135758f1befd1287f683c4753
clevelandtwal <69008094+clevelandtwal@users.noreply.github.com>
2020-09-24 22:14:05 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var questions = [
-    {
-        question: "What is jQuery?",
-        options: ["A lightweight Javascript library", "A free and open-source CSS framework", "A dynamic computer programming language", "A cross-platform web browser developed by Google"],
-        correct: "A lightweight Javascript library"
-    }
-]
